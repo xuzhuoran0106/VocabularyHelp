189fbe3b0d63cdd739557e44b7a341d41414105a
xuzhuoran0106 <xuzhuoran@xus-Mac-mini.local>
2014-06-18 16:25:12 +0900

    Initial Commit

---
+++ VocabularyHelp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VocabularyHelp
+//
+//  Created by xu zhuoran on 6/18/14.
+//  Copyright (c) 2014 xu zhuoran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VocabularyHelp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VocabularyHelp
+//
+//  Created by xu zhuoran on 6/18/14.
+//  Copyright (c) 2014 xu zhuoran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
